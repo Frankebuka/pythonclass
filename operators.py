564eadd96ab78564807c43645509355adfe1c35d
Frankebuka <frankebuka@yahoo.com>
2023-11-22 17:25:27 +0000

    first commit

---
+++ operators.py
@@ -0,0 +1,27 @@
+a = 400
+b = 40
+c = 2
+
+age = 30
+year = 2023
+
+print(str(age), "+", str(year), "=", age + year)
+print(str(age), "-", str(year), "=", age - year)
+print(str(age), "*", str(year), "=", age * year)
+print(str(age), "/", str(year), "=", age / year)
+print(str(age), "%", str(year), "=", age % year)
+print(str(c), "*", "3", "=", c * 3)
+
+Num1 = "40"
+Num2 = "100"
+AddResult = int(Num1) + int(Num2)
+MutiplyResult = int(Num1) - int(Num2)
+print("The result for Addition is =", AddResult)
+print("The result for Multiplication is =", MutiplyResult)
+
+print(age + year)
+print(age - year)
+print(age * year)
+print(age / year)
+print(age % year)
+print(c ** 3)
